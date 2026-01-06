659e71a0b6a9c841dfb2ad22b649cb7a81dd10ef
nadav <nadav@eazix.io>
2026-01-06 11:43:45 +0800

    Initial Android wrapper (Capacitor) setup

---
+++ android/app/src/main/java/com/eazix/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.eazix.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.eazix.app',
+  appName: 'Eazix',
+  webDir: 'dist/mean-corse-01'
+};
+
+export default config;
